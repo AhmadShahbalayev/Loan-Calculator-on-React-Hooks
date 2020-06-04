d370797d4d509e2f5b29a5857a6addec9c863cda
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-06-04 14:07:46 +0400

    Initial upload by Ahmad

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Libraries:
+import React from 'react';
+
+export default () => {
+  return (
+    <React.Fragment>
+      <h1>THIS IS APP JSX</h1>
+    </React.Fragment>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Libraries:
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Tools:
+import './index.css';
+
+// Componenets:
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
