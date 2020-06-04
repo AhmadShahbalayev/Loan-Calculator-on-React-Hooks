ec2f8b9a832d287456f40dd1f8cf7942c0582cc9
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-06-04 17:31:42 +0400

    Project ready. Without CSS by Ahmad & Mahammad

--- src/components/LoanTable.jsx
+++ src/components/LoanTable.jsx
@@ -6,64 +6,70 @@ export default props => {
     const { amount, time, rate } = props.values;
 
     // Parsing values:
-    const parsedDebt = parseFloat(amount);
-    const parsedTime = parseFloat(time);
+    const parsedAmount = parseFloat(amount);
+    const parsedTime = parseInt(time);
     const parsedRate = parseFloat(rate);
 
-    const amortize = (parsedDebt, parsedTime, parsedRate) => {
+    const amortize = (parsedAmount, parsedTime, parsedRate) => {
         // Calculating payment per month:
-        const monthlyRate = parsedRate / 12;
-        const payment = parsedDebt * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -parsedTime)));
+        const monthlyRate = parsedRate / 1200;
+        const payment = parsedAmount * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -parsedTime)));;
 
         // Array to map tbody:
         const tableBody = [];
 
         for (let i = 0; i < parsedTime; i++) {
-            let calculatedInterest = 0;
+            let interest = 0;
             let monthlyPrincipal = 0;
 
-            calculatedInterest = +(+parsedDebt * monthlyRate).toFixed(2);
-            monthlyPrincipal = (payment - calculatedInterest).toFixed(2);
+            interest = +(parsedAmount * monthlyRate);
+            monthlyPrincipal = +(payment - interest);
 
             let obj = {
                 month: i + 1,
                 payment: payment.toFixed(2),
-                debt: parsedDebt.toFixed(2),
-                rate: calculatedInterest,
-                principal: monthlyPrincipal
+                debt: parsedAmount.toFixed(2),
+                rate: interest.toFixed(2),
+                principal: monthlyPrincipal.toFixed(2)
             }
 
+            parsedAmount = parsedAmount - monthlyPrincipal;
             tableBody.push(obj);
-            parsedDebt -= monthlyPrincipal;
         }
 
         return tableBody;
     }
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Month</th>
-                    <th>Payment</th>
-                    <th>Principal</th>
-                    <th>Interest</th>
-                    <th>Debt</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    amortize(parsedDebt, parsedTime, parsedRate).map(row => {
-                        return <tr>
-                            <td>{row.month}</td>
-                            <td>{row.payment}</td>
-                            <td>{row.principal}</td>
-                            <td>{row.rate}</td>
-                            <td>{row.debt}</td>
-                        </tr>
-                    })
-                }
-            </tbody>
-        </table>
+        <React.Fragment>
+            <ul>
+                <li>Loan amount: {parsedAmount.toFixed(2)}</li>
+                <li>Interest rate: {(parsedRate).toFixed(2)}%</li>
+            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Month</th>
+                        <th>Monthly Payment</th>
+                        <th>Principal</th>
+                        <th>Interest</th>
+                        <th>Debt</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        amortize(parsedAmount, parsedTime, parsedRate).map(row => {
+                            return <tr key={row.month}>
+                                <td>#{row.month}</td>
+                                <td>${row.payment}</td>
+                                <td>${row.principal}</td>
+                                <td>${row.rate}</td>
+                                <td>${row.debt}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </React.Fragment>
     )
 }
